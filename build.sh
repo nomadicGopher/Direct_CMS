6aef7ce1ae1d174f6abff0a6bdbf1cb80953300d
Jon Vojtush <11096910+nomadicGopher@users.noreply.github.com>
2025-03-28 21:00:42 -0700

    update build

--- build.sh
+++ build.sh
@@ -17,12 +17,13 @@ if [ $status -ne 0 ]; then
 fi
 echo "web/go.wasm was built." | tee -a $LOG_FILE
 
-echo "Removing web/wasm_exec.js if it exists..." | tee -a $LOG_FILE
-rm -f web/wasm_exec.js 2>&1 | tee -a $LOG_FILE >&2
-
-echo "Fetching a new web/wasm_exec.js..." | tee -a $LOG_FILE 
-cp "$(go env GOROOT)/misc/wasm/wasm_exec.js" web/ 2>&1 | tee -a $LOG_FILE >&2  
-echo "web/wasm_exec.js was fetched from \$GOROOT/misc/wasm/" | tee -a $LOG_FILE
+if [ ! -f web/wasm_exec.js ]; then
+    echo "Fetching a new web/wasm_exec.js..." | tee -a $LOG_FILE 
+    cp "$(go env GOROOT)/misc/wasm/wasm_exec.js" web/ 2>&1 | tee -a $LOG_FILE >&2  
+    echo "web/wasm_exec.js was fetched from \$GOROOT/misc/wasm/" | tee -a $LOG_FILE
+else
+    echo "web/wasm_exec.js already exists. Skipping fetch." | tee -a $LOG_FILE
+fi
 
 echo "Build process ended at: $(date)" | tee -a $LOG_FILE
 exit
