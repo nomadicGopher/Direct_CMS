e3a96293e6ab7edf250099d78966ed3b888c4ee0
Jon Vojtush <11096910+nomadicGopher@users.noreply.github.com>
2025-03-29 00:08:33 -0600

    simplify & localize

--- main.go
+++ main.go
@@ -1,40 +1,11 @@
 package main
 
 import (
-	"flag"
 	"fmt"
-	"net/http"
-	"path/filepath"
 )
 
 func main() {
-	flag.Parse()
-
-	http.HandleFunc("/", serveUserResources)
-
-	fmt.Println("Serving user defined files...")
-
-	if err := http.ListenAndServe(":8080", nil); err != nil {
-		fmt.Println("Failed to start server: ", err)
-	}
-
-	fmt.Println("User defined files served.")
+	fmt.Println("Hello from Go via WASM.")
 
 	select {}
-}
-
-func serveUserResources(w http.ResponseWriter, r *http.Request) {
-	filePatterns := []string{"logo.*", "sitemap.xml", "custom.css", "custom.js", "favicon.*"}
-
-	for _, filePattern := range filePatterns {
-		file, err := filepath.Glob(filepath.Join("userContent", filePattern))
-		if err != nil {
-			http.Error(w, fmt.Sprintf("Error finding file pattern %s: %v", filePattern, err), http.StatusInternalServerError)
-			continue
-		}
-
-		fmt.Println("Serving: 'userContent/" + file[0] + "'")
-
-		http.ServeFile(w, r, "userContent/"+file[0])
-	}
 }
