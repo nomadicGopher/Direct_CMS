6e03a756f12e6af9e17aae11286752228e50c0dc
Jon Vojtush <11096910+nomadicGopher@users.noreply.github.com>
2025-03-30 19:14:08 -0600

    identify bug

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import (
 )
 
 func main() {
-	fmt.Println("Hello from Go via WASM.")
-
-	select {}
+	fmt.Println("This returns OK in the DOM.")
+	fmt.Println("This does NOT return OK in the DOM.") //!
+	select {}                                          // Keep go running in the background via WASM.
 }
